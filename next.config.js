0682dec1cf6f6bc31690c8df438e907b2198266f
sadf238 <48848543+sadf238@users.noreply.github.com>
2026-06-19 20:20:01 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
-  },
-  headers: function() {
-    return Promise.resolve([
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'referrer-policy',
-            value: 'no-referrer',
-          }
-        ]
-      }
-    ]);
   }
 };
 
